5c19174aa49b03356a9af9be80cbc6f3304bd07e
dongree <dongwoo0307@naver.com>
2020-12-24 20:54:02 +0900

    add goodAndBad function

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
 const moreBtn = document.querySelector('.info .metadata .moreBtn');
 const title = document.querySelector('.info .metadata .title');
 
+const good = document.querySelector('.info .button_list .good');
+const bad = document.querySelector('.info .button_list .bad');
+
+
 moreBtn.addEventListener('click', () => {
     moreBtn.classList.toggle('clicked');
     title.classList.toggle('clamp');
 });
+
+
+good.addEventListener('click', () =>{
+    good.classList.toggle('clicked');
+})
+
+bad.addEventListener('click', () =>{
+    bad.classList.toggle('clicked');
+})
